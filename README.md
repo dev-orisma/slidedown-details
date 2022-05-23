50afec9ba066928f052490538bd055be333d66db
Kasithas (Name) <kasithas@orisma.com>
2022-05-23 17:15:42 +0700

    Init

---
+++ README.md
@@ -0,0 +1,94 @@
+# Slidedown
+
+## Introduction
+Make slidedown with native HTML tag `<details>`
+
+**pros**
+- Native HTML
+- No Javascript Requieded to toggle open/close event
+
+<br>
+
+**Cons**
+- No fancy UI - Must custom UI
+
+<br>
+
+## Feature
+- `<details>` can access offsetHeight, include tag `<summary>`.
+- support [events](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details#events) <code>toggle</code>
+    ```javascript
+    details.addEventListener("toggle", event => {
+        if (details.open) {
+            /* the element was toggled open */
+        } else {
+            /* the element was toggled closed */
+        }
+    });
+    ```
+
+<br>
+
+## Browser Support
+- [HTML `<details>`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details#browser_compatibility)
+- [HTML `<summary>`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/summary#browser_compatibility)
+- [Javascript - requestAnimationFrame](https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame#browser_compatibility)
+
+<br>
+
+## Example
+### HTML
+This example shows a simple `<details>` element with a `<summary>`
+```html
+<details>
+    <summary>Heading</summary>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+</details>
+```
+![Native HTML tag details!](/images/native.gif "San Juan Mountains")
+
+<br>
+
+---
+## Use Libery Javascript
+Manual init
+```javascript
+var slidedowns = document.querySelectorAll('[slidedown-details]');
+var options = {
+    duration: 300,
+    easing: 'ease'
+}
+slidedowns.forEach((el) => {
+    new SlidedownDetails(el, options);
+});
+```
+
+Auto init
+```javascript
+var options = {
+    duration: 300,
+}
+new SlidedownDetails(options);
+// or
+new SlidedownDetails();
+```
+
+Auto init - Custom selector `(comming soon)`
+```javascript
+var slidedowns = document.querySelectorAll('[slidedown-details]');
+var options = {
+    duration: 300,
+}
+new SlidedownDetails(slidedowns);
+// or
+new SlidedownDetails(slidedowns, options);
+```
+![Add Javascript with tag details!](/images/libs.gif "San Juan Mountains")
+<br>
+
+## Access Value
+*`_slidedownArray`* - Array
+
+Access with globla value: `_slidedownArray`.
+
+<br>
---
+++ resource/js/slidedown-details.js
@@ -0,0 +1,165 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // document.querySelectorAll('[slidedown-details]').forEach((el) => {
+    //     var options = {
+    //         duration: 300,
+    //     }
+    //     new SlidedownDetails(el, options);
+    // });
+    
+    // =====
+    // var options = {
+    //     duration: 300,
+    // }
+    // new SlidedownDetails(options);
+    
+    // =====
+    // var options = {
+    //     duration: 300,
+    // }
+    // new SlidedownDetails(document.querySelectorAll('[slidedown-details]'), options);
+    new SlidedownDetails(document.querySelectorAll('[slidedown-details]'));
+});
+_slidedownArray = [];
+function SlidedownDetails() {
+    var element = arguments[0];
+    var options = arguments[1];
+
+    this.selector = 'slidedown-details';
+
+    if (typeof element === 'object' && element.nodeType !== undefined) {
+
+        this.opts = this.extend({
+            duration: 400,
+            easing: 'ease-in-out',
+            class: 'default-theme'
+        }, options);
+
+        this.init(element);
+
+    } else {
+
+        options = arguments[0];
+        document.querySelectorAll('[' + this.selector + ']').forEach(function(elements) {
+            var w = new SlidedownDetails(elements, options);
+            _slidedownArray.push(w);
+        });
+
+    }
+}
+
+SlidedownDetails.prototype.init = function (element) {
+    // console.log(element);
+    if (!this.isValidAttribute(element))
+        return;
+
+    element.classList.add(this.opts.class);
+    this.el = element.querySelector('details');
+    this.summary = this.el.querySelector('summary');
+    this.content = this.el.querySelector('.content');
+    this.animate = null;
+    this.isClose = false;
+    this.isOpen = false;
+    this.summary.addEventListener('click', function(e) { 
+        this.onClick(e); 
+    }.bind(this));
+};
+SlidedownDetails.prototype.onClick = function(event) {
+    event.preventDefault();
+    this.el.style.overflow = 'hidden';
+    if (this.isClose || !this.el.open) {
+        this.slidedown();
+    } else if (this.isOpen || this.el.open) {
+        this.slideup();
+    }
+};
+SlidedownDetails.prototype.slideup = function() {
+    var that = this;
+    this.isClose = true;
+
+    var startHeight = this.el.offsetHeight + 'px';
+    var endedHeight = this.summary.offsetHeight + 'px';
+
+    if (this.animate) {
+        this.animate.cancel();
+    }
+
+    this.animate = this.el.animate({
+        height: [startHeight, endedHeight]
+    }, {
+        duration: that.opts.duration,
+        easing: that.opts.easing
+    });
+
+    this.animate.onfinish = function() {
+        that.onAnimationFinish(false);
+    };
+    this.animate.oncancel = function() {
+        that.isClose = false;
+    };
+};
+SlidedownDetails.prototype.slidedown = function() {
+    var that = this;
+    this.el.style.height = `${this.el.offsetHeight}px`;
+    this.el.open = open;
+    window.requestAnimationFrame(() => that.onOpen());
+};
+SlidedownDetails.prototype.onOpen = function() {
+    var that = this;
+    this.isOpen = true;
+
+    var startHeight = this.el.offsetHeight + 'px';
+    var endedHeight = this.summary.offsetHeight + this.content.offsetHeight + 'px';
+
+    if (this.animate) {
+        this.animate.cancel();
+    }
+    this.animate = this.el.animate({
+        height: [startHeight, endedHeight]
+    }, {
+        duration: that.opts.duration,
+        easing: that.opts.easing
+    });
+
+    this.animate.onfinish = function() {
+        that.onAnimationFinish(true);
+    };
+    this.animate.oncancel = function() {
+        that.isOpen = false;
+    };
+};
+SlidedownDetails.prototype.onAnimationFinish = function(open) {
+    this.el.open = open;
+    this.animate = null;
+    this.isClose = false;
+    this.isOpen = false;
+    this.el.style.height = '';
+    this.el.style.overflow = '';
+};
+SlidedownDetails.prototype.isValidAttribute = function(selector) {
+    return selector.hasAttribute('slidedown-details');
+};
+SlidedownDetails.prototype.extend = function(options) {
+    if (typeof options === 'undefined' || typeof options !== 'object') 
+        options = {};
+
+    for (var i = 1; i < arguments.length; i++) {
+        var obj = arguments[i];
+
+        if (!obj) continue;
+
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                if (typeof obj[key] === "object" && obj[key] !== null) {
+                    if (obj[key] instanceof Array) {
+                        options[key] = obj[key].slice(0);
+                    } else {
+                        options[key] = this.extend(options[key], obj[key]);
+                    }
+                } else {
+                    options[key] = obj[key];
+                }
+            }
+        }
+    }
+    return options;
+};
